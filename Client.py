1c8d747315933f7436b7b8eb03f9aa725bef1808
Ryan <ryanlim43@hotmail.com>
2016-10-10 14:55:42 +0800

    updated

--- Client.py
+++ Client.py
@@ -6,30 +6,27 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 5555)
 
-rate_bits_per_sec = 100000000
+rate_bits_per_sec = 1000000
 rate_bytes_per_sec = rate_bits_per_sec/8
 lead_seg_id = 0
-sleep_time = 0
+# sleep_time = 0
 message = (str(lead_seg_id) + " This is the message.  It will be repeated,")
 
 try:
-    sent = sock.sendto(("1"), server_address)
-    time.sleep(0.5)
+    # sent = sock.sendto(("1"), server_address)
+    # time.sleep(0.5)
     # For Sending Rate to stabalise
     while True:
-        lead_seg_id = 0
         # Sending at 1.5 Mb/s
-        while(lead_seg_id < rate_bytes_per_sec + len(message)):
-            message = (str(lead_seg_id) + " This is the message.  It will be repeated,")
-            sent = sock.sendto((message), server_address)
-            lead_seg_id += len(message)
-            sleep_time += float(len(message))/rate_bytes_per_sec
-            time.sleep(float(len(message))/rate_bytes_per_sec)
+        # while(lead_seg_id < rate_bytes_per_sec + len(message)):
+        message = (str(lead_seg_id) + " This is the message.  It will be repeated,")
+        sent = sock.sendto((message), server_address)
+        lead_seg_id += 1
+        # sleep_time += float(len(message))/rate_bytes_per_sec
+        time.sleep(float(len(message))/rate_bytes_per_sec)
 
 finally:
     print ('Closing socket')
-    sock.close(
-    )
+    sock.close()
 
-print ('done')
 print('done')
--- Server.py
+++ Server.py
@@ -11,25 +11,35 @@ print >> sys.stderr, 'starting up on %s port %s' % server_address
 print('\nWaiting to receive message...')
 sock.bind(server_address)
 total_received_bytes = 0
-
-while True:
-    data, address = sock.recvfrom(4096)
-    if len(data) == 1:
-        timestamp = time.time()
-        break
+prev_lead_seg_id = 0
+missing_packets = []
+# while True:
+#     data, address = sock.recvfrom(4096)
+#     if len(data) == 1:
+#         timestamp = time.time()
+#         break
 
 # Sleep else there will be float divison by zero error
-time.sleep(0.5)
+# time.sleep(0.5)
 
 while True:
     data, address = sock.recvfrom(4096)
+    lead_seg_id = int(data.split()[0])
+    prev_lead_seg_id+=1
+    if (lead_seg_id != prev_lead_seg_id):
+        if (lead_seg_id in missing_packets):
+            missing_packets.remove(lead_seg_id)
+        else:
+            missing_packets.append(prev_lead_seg_id+1)
+        if (missing_packets[0] < lead_seg_id - 10):
+            print('Packet #%d has been dropped.' %(lead_seg_id) )
 
-    donestamp = time.time()
-    data = len(data)
-    total_received_bytes += data
-
-    rate = (total_received_bytes / (donestamp - timestamp))
-    print "\nRcvd: %s bytes, %s total in %s s at %s bytes per second" % (data, total_received_bytes, donestamp - timestamp, rate)
+    # donestamp = time.time()
+    # data = len(data)
+    # total_received_bytes += data
+    #
+    # rate = (total_received_bytes / (donestamp - timestamp))
+    # print "\nRcvd: %s bytes, %s total in %s s at %s bytes per second" % (data, total_received_bytes, donestamp - timestamp, rate)
 
 
     
